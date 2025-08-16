d8b82e3e0ebf50e6d7feed702ea542fd71f5b379
Adam Croft <169613052+Adock90@users.noreply.github.com>
2025-08-16 18:31:05 +0100

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-$(gcc -shared -o ../libs/AutoCalcMath.Fraction.so -fPIC AutoCalcMath.Fraction.c)
-$(gcc -shared -o ../libs/AutoCalcMath.Ratio.so -fPIC AutoCalcMath.Ratio.c)
-$(gcc -shared -o ../libs/AutoCalcMath.Simple.so -fPIC AutoCalcMath.Simple.c) 
-$(gcc -shared -o ../libs/AutoCalcMath.Trigonomitry.so -fPIC AutoCalcMath.Trigonomitry.c)
-
